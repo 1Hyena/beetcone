f4d7a16b02d9406cf80a7c56cfb43e0bf1e7ebf2
1Hyena <mail@ericherstu.net>
2026-09-18 14:04:34 +0300

    project structure

---
+++ tools/src/bc2addr.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+#include "../../beetcone.h"
+
+int main(int, char **) {
+    return EXIT_SUCCESS;
+}
---
+++ tools/src/utils.h
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+#ifndef UTILS_H_18_09_2026
+#define UTILS_H_18_09_2026
+
+
+#endif
